81a7a2e58184e6373e4a62996b1de2d51b6ee6e4
fuadajip <fuadajipratomo@gmail.com>
2018-11-20 10:24:56 +0700

    add complex metas in helmet

--- src/app/About/index.js
+++ src/app/About/index.js
@@ -7,7 +7,33 @@ class About extends Component {
       <React.Fragment>
         <Helmet>
           <title>About</title>
-          <meta name="description" content="About page" />
+          <meta
+            name="description"
+            content="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+          />
+          <meta
+            itemprop="image"
+            content="https://allabouttheatre.org/wp-content/uploads/2018/03/Simba.jpg"
+          />
+          <meta property="og:title" content="About" />
+          <meta
+            property="og:image"
+            content="https://allabouttheatre.org/wp-content/uploads/2018/03/Simba.jpg"
+          />
+          <meta
+            property="og:description"
+            content="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+          />
+          <meta name="twitter:title" content="About" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:description"
+            content="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+          />
+          <meta
+            name="twitter:image"
+            content="https://allabouttheatre.org/wp-content/uploads/2018/03/Simba.jpg"
+          />
         </Helmet>
         <div>About page</div>
       </React.Fragment>
--- src/app/Home/index.js
+++ src/app/Home/index.js
@@ -7,7 +7,33 @@ class Home extends Component {
       <React.Fragment>
         <Helmet>
           <title>Home</title>
-          <meta name="description" content="Home page" />
+          <meta
+            name="description"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+          />
+          <meta
+            itemprop="image"
+            content="https://allabouttheatre.org/wp-content/uploads/2018/03/Simba.jpg"
+          />
+          <meta property="og:title" content="Home" />
+          <meta
+            property="og:image"
+            content="https://allabouttheatre.org/wp-content/uploads/2018/03/Simba.jpg"
+          />
+          <meta
+            property="og:description"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+          />
+          <meta name="twitter:title" content="Home" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:description"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+          />
+          <meta
+            name="twitter:image"
+            content="https://allabouttheatre.org/wp-content/uploads/2018/03/Simba.jpg"
+          />
         </Helmet>
         <div>Home page</div>
       </React.Fragment>
